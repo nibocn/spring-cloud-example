3dcb6b42f506b03985e8d6bafa868e59647df32d
NiBo <nibocn@gmail.com>
2020-10-29 17:33:25 +0800

    feat(registry): Add registry service

---
+++ registry/src/main/java/me/nibo/springcloud/registry/RegistryApplication.java
@@ -0,0 +1,20 @@
+package me.nibo.springcloud.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心入口类
+ *
+ * @author NiBo
+ */
+@SuppressWarnings({ "PMD.UseUtilityClass" })
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
